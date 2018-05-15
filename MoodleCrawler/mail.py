68e7f680da910882ae15a8ba2326439edb050f1f
Linwenye <419766878@qq.com>
2018-05-15 12:34:12 +0800

    mail sending problem

--- MoodleCrawler/mail.py
+++ MoodleCrawler/mail.py
@@ -5,13 +5,10 @@ import smtplib
 # from email.mime.text import MIMEText
 from email.message import EmailMessage
 
+changed_courses = {}
+
 user = config.mailname
 password = config.mailpass
-server_ssl = smtplib.SMTP_SSL(settings.MAIL_HOST, settings.MAIL_PORT)
-server_ssl.ehlo()  # optional, called by login()
-server_ssl.login(user, password)
-
-changed_courses = {}
 
 
 def send_mail(title, recipient, course_key, message=None):
@@ -34,6 +31,10 @@ def _send_mail(title, message, recipient):
 
     msg.set_content(message)
     # msg.attach(MIMEText(message, 'plain', 'utf-8'))
+
+    server_ssl = smtplib.SMTP_SSL(settings.MAIL_HOST, settings.MAIL_PORT)
+    server_ssl.ehlo()  # optional, called by login()
+    server_ssl.login(user, password)
 
     # ssl server doesn't support or need tls, so don't call server_ssl.starttls()
     server_ssl.send_message(from_addr=user, to_addrs=recipient, msg=msg)
